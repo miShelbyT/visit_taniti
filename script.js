362477268abccaffcc781fdb36dc19ffd85aae9b
MiShelby Talbert <mi.shelbyrose@gmail.com>
2024-11-25 15:56:37 -0500

    adding all final images and setting credits, plus a little js for 'learn more' elements

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 
 const closeBtns = document.querySelectorAll(".container small");
+const learnMore = document.querySelectorAll(".parent a");
 
 closeBtns.forEach((btn)=> {
   btn.addEventListener("click", (e)=> {
     const parent = e.target.closest(".container")
     parent.remove()
   })
+})
+
+learnMore.forEach(el => {
+  el.addEventListener("click", () => window.alert("This feature is still under construction. Please come back later. Thank you."))
 })
