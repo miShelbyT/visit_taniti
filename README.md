fef8c44b3d448dd9cc24cb466a0b75b96c98bcc1
MiShelby Talbert <mi.shelbyrose@gmail.com>
2024-11-25 16:57:36 -0500

    fixed spacing in flexbox header and added functionality to select or deselect images as travel options for Plan Your Visit

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # Visit Taniti
-UX Design Course for WGU - prototype
+UX Design Course for WGU
 
-This project was created with HTML and CSS.
+This project was created with HTML, CSS and a touch of JavaScript. Contains a prototype product for a fictional island called Taniti, located in the Pacific.
+
+TODOS:
+1. add selected class with some sort of outline and box shadow to elements
--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 const closeBtns = document.querySelectorAll(".container small");
 const learnMore = document.querySelectorAll(".parent a");
+const images = document.querySelectorAll(".option img")
 
 closeBtns.forEach((btn)=> {
   btn.addEventListener("click", (e)=> {
@@ -11,4 +12,12 @@ closeBtns.forEach((btn)=> {
 
 learnMore.forEach(el => {
   el.addEventListener("click", () => window.alert("This feature is still under construction. Please come back later. Thank you."))
+})
+
+images.forEach(img => {
+  img.addEventListener("click", (e) => {
+    const parent = e.target.closest(".container")
+    parent.classList.contains("selected") ? parent.classList.remove("selected") : parent.classList.add("selected");
+  })
+
 })
